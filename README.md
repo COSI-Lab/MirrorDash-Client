09ac4e8cf8109dd0e60e5a5e3dd7cf7b1920796f
Benjamin Lannon <lannonbr@clarkson.edu>
2017-11-11 09:48:52 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MirrorDash-Client
+
+The React Frontend for the 3rd iteration of the MirrorBand Project
---
+++ src/util.js
@@ -0,0 +1,13 @@
+export function minimizeBytes(bytes) {
+    let res = bytes;
+
+    let i = 0;
+    const sizeArr = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+
+    while(res / 1000 > 1) {
+        res /= 1000.0;
+        i++;
+    }
+
+    return res.toFixed(2) + sizeArr[i];
+}
