ca0c29e5ef23d7a037086f4904b83fd4e1cc6506
Benjamin Lannon <lannonbr@clarkson.edu>
2018-02-09 23:12:49 -0500

    condensing boxes and cleaning up distrousage file

---
+++ src/Components/repoImages.js
@@ -0,0 +1,76 @@
+import ubuntuLogo from "../img/logo/ubuntu-logo32.png";
+import blenderlogo from "../img/logo/blender_logo.png";
+import linuxmintLogo from "../img/logo/linuxmint_logo.png";
+import libreofficeLogo from "../img/logo/libreoffice_logo.png";
+import opensuseLogo from "../img/logo/opensuse_logo.jpg";
+import centosLogo from "../img/logo/centos_logo.png";
+import parrotsecLogo from "../img/logo/parrotsec_logo.png";
+import vlcLogo from "../img/logo/vlc_logo.png";
+import cygwinLogo from "../img/logo/cygwin_logo.png";
+import archlinuxLogo from "../img/logo/archlinux_logo.png";
+import debianLogo from "../img/logo/debian_logo.png";
+import gnuLogo from "../img/logo/gnu_logo.png";
+import ipfireLogo from "../img/logo/ipfire_logo.png";
+import manjaroLogo from "../img/logo/manjaro_logo.png";
+import sabayonLogo from "../img/logo/sabayon_logo.png";
+import slackwareLogo from "../img/logo/slackware_logo.png";
+import voidLinuxLogo from "../img/logo/void_logo.png";
+import odroidLogo from "../img/logo/odroid_logo.png";
+import gpartedLogo from "../img/logo/gparted_logo.png";
+import cpanLogo from "../img/logo/cpan_logo.png";
+import texLogo from "../img/logo/tex_logo.jpg";
+import rLogo from "../img/logo/r_logo.png";
+import gentooLogo from "../img/logo/gentoo_logo.png";
+import fedoraLogo from "../img/logo/fedora_logo.png";
+import isabelleLogo from "../img/logo/isabelle_logo.gif";
+import openbsdLogo from "../img/logo/openbsd_logo.png";
+import rosLogo from "../img/logo/ros_logo.png";
+import linuxLogo from "../img/logo/linux_logo.png";
+import clonezillaLogo from "../img/logo/clonezilla_logo.jpg";
+import raspbianLogo from "../img/logo/raspbian_logo.png";
+import freebsdLogo from "../img/logo/freebsd_logo.png";
+import slitazLogo from "../img/logo/slitaz_logo.png";
+
+const repoImages = {
+  blender: blenderlogo,
+  ubuntu: ubuntuLogo,
+  linuxmint: linuxmintLogo,
+  tdf: libreofficeLogo,
+  opensuse: opensuseLogo,
+  centos: centosLogo,
+  "ubuntu-releases": ubuntuLogo,
+  parrot: parrotsecLogo,
+  videolan: vlcLogo,
+  cygwin: cygwinLogo,
+  archlinux: archlinuxLogo,
+  debian: debianLogo,
+  manjaro: manjaroLogo,
+  sabayon: sabayonLogo,
+  ipfire: ipfireLogo,
+  voidlinux: voidLinuxLogo,
+  gnu: gnuLogo,
+  slackware: slackwareLogo,
+  "debian-cd": debianLogo,
+  "debian-security": debianLogo,
+  "ubuntu-ports": ubuntuLogo,
+  "ubuntu-cdimage": ubuntuLogo,
+  odroid: odroidLogo,
+  gparted: gpartedLogo,
+  cpan: cpanLogo,
+  ctan: texLogo,
+  cran: rLogo,
+  gentoo: gentooLogo,
+  "gentoo-portage": gentooLogo,
+  fedora: fedoraLogo,
+  "fedora-epel": fedoraLogo,
+  isabelle: isabelleLogo,
+  openbsd: openbsdLogo,
+  ros: rosLogo,
+  linux: linuxLogo,
+  clonezilla: clonezillaLogo,
+  raspbian: raspbianLogo,
+  freebsd: freebsdLogo,
+  slitaz: slitazLogo
+};
+
+export default repoImages;
--- src/Pages/DistroUsage.js
+++ src/Pages/DistroUsage.js
@@ -3,39 +3,7 @@ import React, { Component } from "react";
 import Lokka from "lokka";
 import { Transport } from "lokka-transport-http";
 import SlideInDiv from "../Components/SlideInDiv";
-
-import ubuntuLogo from "../img/logo/ubuntu-logo32.png";
-import blenderlogo from "../img/logo/blender_logo.png";
-import linuxmintLogo from "../img/logo/linuxmint_logo.png";
-import libreofficeLogo from "../img/logo/libreoffice_logo.png";
-import opensuseLogo from "../img/logo/opensuse_logo.jpg";
-import centosLogo from "../img/logo/centos_logo.png";
-import parrotsecLogo from "../img/logo/parrotsec_logo.png";
-import vlcLogo from "../img/logo/vlc_logo.png";
-import cygwinLogo from "../img/logo/cygwin_logo.png";
-import archlinuxLogo from "../img/logo/archlinux_logo.png";
-import debianLogo from "../img/logo/debian_logo.png";
-import gnuLogo from "../img/logo/gnu_logo.png";
-import ipfireLogo from "../img/logo/ipfire_logo.png";
-import manjaroLogo from "../img/logo/manjaro_logo.png";
-import sabayonLogo from "../img/logo/sabayon_logo.png";
-import slackwareLogo from "../img/logo/slackware_logo.png";
-import voidLinuxLogo from "../img/logo/void_logo.png";
-import odroidLogo from "../img/logo/odroid_logo.png";
-import gpartedLogo from "../img/logo/gparted_logo.png";
-import cpanLogo from "../img/logo/cpan_logo.png";
-import texLogo from "../img/logo/tex_logo.jpg";
-import rLogo from "../img/logo/r_logo.png";
-import gentooLogo from "../img/logo/gentoo_logo.png";
-import fedoraLogo from "../img/logo/fedora_logo.png";
-import isabelleLogo from "../img/logo/isabelle_logo.gif";
-import openbsdLogo from "../img/logo/openbsd_logo.png";
-import rosLogo from "../img/logo/ros_logo.png";
-import linuxLogo from "../img/logo/linux_logo.png";
-import clonezillaLogo from "../img/logo/clonezilla_logo.jpg";
-import raspbianLogo from "../img/logo/raspbian_logo.png";
-import freebsdLogo from "../img/logo/freebsd_logo.png";
-import slitazLogo from "../img/logo/slitaz_logo.png";
+import repoImages from "../Components/repoImages";
 
 import "../../node_modules/react-grid-layout/css/styles.css";
 import "../../node_modules/react-resizable/css/styles.css";
@@ -48,52 +16,8 @@ const client = new Lokka({
   transport: new Transport("http://localhost:4000/graphql")
 });
 
-const distroImages = {
-  blender: blenderlogo,
-  ubuntu: ubuntuLogo,
-  linuxmint: linuxmintLogo,
-  tdf: libreofficeLogo,
-  opensuse: opensuseLogo,
-  centos: centosLogo,
-  "ubuntu-releases": ubuntuLogo,
-  parrot: parrotsecLogo,
-  videolan: vlcLogo,
-  cygwin: cygwinLogo,
-  archlinux: archlinuxLogo,
-  debian: debianLogo,
-  manjaro: manjaroLogo,
-  sabayon: sabayonLogo,
-  ipfire: ipfireLogo,
-  voidlinux: voidLinuxLogo,
-  gnu: gnuLogo,
-  slackware: slackwareLogo,
-  "debian-cd": debianLogo,
-  "debian-security": debianLogo,
-  "ubuntu-ports": ubuntuLogo,
-  "ubuntu-cdimage": ubuntuLogo,
-  odroid: odroidLogo,
-  gparted: gpartedLogo,
-  cpan: cpanLogo,
-  ctan: texLogo,
-  cran: rLogo,
-  gentoo: gentooLogo,
-  "gentoo-portage": gentooLogo,
-  fedora: fedoraLogo,
-  "fedora-epel": fedoraLogo,
-  isabelle: isabelleLogo,
-  openbsd: openbsdLogo,
-  ros: rosLogo,
-  linux: linuxLogo,
-  clonezilla: clonezillaLogo,
-  raspbian: raspbianLogo,
-  freebsd: freebsdLogo,
-  slitaz: slitazLogo
-};
-
 const DistroContainer = styled.div`
   display: flex;
-  /* flex-direction: column; */
-  /* justify-content: space-between; */
   align-items: center;
   padding: 10px;
   height: 40px;
@@ -106,7 +30,7 @@ const DistroContainer = styled.div`
   h3 {
     margin-top: 0;
     margin-bottom: 10px;
-    font-size: 30px;
+    font-size: 20px;
     text-align: left;
   }
 
@@ -116,7 +40,14 @@ const DistroContainer = styled.div`
 
   img {
     height: auto;
-    width: 120px;
+    width: 60px;
+  }
+
+  .movement {
+    position: absolute;
+    right: 10px;
+    color: green;
+    bottom: 10px;
   }
 `;
 
@@ -125,23 +56,19 @@ const Distro = props => {
   return (
     <DistroContainer style={props.style} key={props.key}>
       <img
-        src={distroImages[distro]}
+        src={repoImages[distro]}
         alt="logo"
         style={{
-          position: "relative",
-          top: "0px",
           paddingRight: 20
         }}
       />
       <div>
-        <h3>{distro}</h3>
+        <h3>
+          {props.num}: {distro}
+        </h3>
         <span>{minimizeBytes(bytes)}</span>
       </div>
-      {/* <h3>
-        {props.num}: {distro}
-      </h3>
-
-      <span>{minimizeBytes(bytes)}</span> */}
+      <div className="movement">&#9650;1</div>
     </DistroContainer>
   );
 };
@@ -190,8 +117,8 @@ class DistroUsage extends Component {
         {this.state.distrousage && (
           <ReactGridLayout
             className="layout"
-            cols={3}
-            rowHeight={100}
+            cols={4}
+            rowHeight={60}
             width={1200}
             margin={[30, 30]}
             isResizable={false}
@@ -202,8 +129,7 @@ class DistroUsage extends Component {
                 num={x + 1}
                 distro={distro}
                 key={x}
-                data-grid={{ x: x % 3, y: Math.ceil(x / 3), w: 1, h: 1 }}
-                logo={ubuntuLogo}
+                data-grid={{ x: x % 4, y: Math.ceil(x / 4), w: 1, h: 1 }}
               />
             ))}
           </ReactGridLayout>
