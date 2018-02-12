956794a170858420dda9bc06b09fca8928e8016a
Benjamin Lannon <lannonbr@clarkson.edu>
2018-02-11 19:28:00 -0500

    Quick bug fix

--- src/Components/GridContainer.js
+++ src/Components/GridContainer.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import "./GridContainer.css";
 
 const GridContainer = props => (
-  <div className="gridContainer">{this.props.children}</div>
+  <div className="gridContainer">{props.children}</div>
 );
 
 export default GridContainer;
